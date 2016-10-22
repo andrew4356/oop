08ef908aa6a8338e8cde93a70bb3c058753a1874
andrew4356 <pop.andrei.212@gmail.com>
2016-10-22 09:02:14 +0300

    Замена на range-based for

--- lw2/Encode/Encode/main.cpp
+++ lw2/Encode/Encode/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,21 +6,20 @@
 
 using namespace std;
 
-string HtmlEncode(string const & currentStr)
+string htmlencode(string const & currentstr)
 {
 	string buffer = "";
-	buffer.reserve(currentStr.size());
-	for (size_t i = 0; i != currentStr.size(); ++i)
+	buffer.reserve(currentstr.size());
+	for (const auto ch : currentstr)
 	{
-		switch (currentStr[i])
+		switch (ch)
 		{
 		case '&': buffer.append("&amp;"); break;
 		case '\"': buffer.append("&quot;"); break;
 		case '\'': buffer.append("&apos;"); break;
 		case '<': buffer.append("&lt;"); break;
 		case '>': buffer.append("&gt;"); break;
-		default: buffer.append(&currentStr[i], 1); break;
-	    
+		default: buffer.append(&ch, 1); break;
 		}
 	}
 	
@@ -31,14 +29,42 @@ string HtmlEncode(string const & currentStr)
 int main()
 {
 
-	string currentStr;
+	string currentstr;
 
-	while (getline(cin, currentStr))
+	while (getline(cin, currentstr))
 	{
 		
-		cout << HtmlEncode(currentStr) <<  endl;
+		cout << htmlencode(currentstr) <<  endl;
 	}
 
 	return 0;
 }
+
+//#include <iostream>
+//#include <string>
+// 
+//using namespace std;
+//void Decode(string & text)
+//{
+//	std::string ToReplace = "sda";
+//	std::string ReplaceWith = "xd";
+//	text.replace(text.find(ToReplace), ToReplace.length(), ReplaceWith);
+//}
+////---------------------------------------------------------------------------
+//int main()
+//{
+//	std::string text = "sdadasdasdasdasdop";
+//	
+//	std::cout << "Original string: \t" << text << std::endl;
+//	//size_t i = text.length();
+//	for (size_t pos = 0; (pos = text.find(from, pos)) != std::string::npos; pos += to.size())
+//	{  
+//		++i;
+//		Decode(text);
+//	}
+//	std::cout << "After replacing: \t" << text << std::endl << std::endl;
+//
+//	std::system("pause");
+//	return 0;
+//}
 
